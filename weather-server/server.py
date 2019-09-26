946dde45d341e1750923bf755b024e01a7dc82a5
Cameron <Stampede10343@gmail.com>
2019-09-26 19:14:23 -0400

    Only show data from 24h
    Added form controls to be able to switch unit and to average the
    datapoints to reduce the noise on the graph.

--- weather-server/server.py
+++ weather-server/server.py
@@ -7,6 +7,9 @@ from datetime import datetime
 
 import sqlite3
 
+import numpy as np
+from scipy import signal
+
 app = Flask(__name__)
 
 @app.route('/temperature', methods=['POST'])
@@ -30,14 +33,24 @@ def get_temperature():
 
     connection = get_connection()
     temps = []
-    for temp_reading in connection.execute('''SELECT date, temp FROM temperature ORDER BY date'''):
+    for temp_reading in connection.execute('''SELECT date as d, temp FROM temperature WHERE d >= strftime('%s', 'now', '-1 day') ORDER BY d;'''):
         temp = temp_reading[1]
         temp = temp if unit == 'C' else (temp * 1.8) + 32
         temp_reading = (temp_reading[0], temp)
         temps.append(temp_reading)
-        print(temp_reading)
 
     connection.close()
+
+    filtered_temps = []
+    batch = int(request.args.get('interval', 15))
+    for index in range(batch, len(temps), batch):
+        average_temp = 0
+        average_time = 0
+        for i in range(index - batch, index):
+            average_time = average_time + temps[index][0]
+            average_temp = average_temp + temps[index][1]
+
+        filtered_temps.append((average_time/batch, average_temp/batch))
 
     data = {
         'labels': [],
@@ -49,11 +62,43 @@ def get_temperature():
             }
         ]
     }
-    for temp_reading in temps:
+
+    # shaped = np.array(temps)
+    # shaped.reshape(-1, 3)
+    # shaped.reshape(-1, 3).mean(axis=1)
+    # print(shaped[0])
+
+    for temp_reading in filtered_temps:
         data['labels'].append(datetime.fromtimestamp(temp_reading[0]).strftime('%b, %d %I:%M %p'))
         data['datasets'][0]['data'].append(temp_reading[1])
 
     return render_template('dashboard.html', data=data, unit=unit)
+
+@app.route('/sampled')
+def get_sampled_data():
+    unit = request.args.get('unit', 'C')
+    unit = 'C' if unit != 'F' else unit
+
+    connection = get_connection()
+    temps = []
+    for temp_reading in connection.execute('''SELECT date, temp FROM temperature ORDER BY date'''):
+        temp = temp_reading[1]
+        temp = temp if unit == 'C' else (temp * 1.8) + 32
+        temp_reading = (temp_reading[0], temp)
+        temps.append(temp_reading)
+
+    connection.close()
+
+    datapoints = 100
+    filtered_temps = []
+    while len(temps) % datapoints != 0:
+        temps.append((None, None))
+
+
+    array = np.array(temps).reshape(-1, 2).reshape(-1, datapoints)
+    # array = array.reshape(-1, datapoints).mean(axis=1)
+
+    return jsonify({'shape': len(array.shape), 'all':array.size, 'yee': str(array)})
 
 def get_connection():
     return sqlite3.connect('temperature.db')
